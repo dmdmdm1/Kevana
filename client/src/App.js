715014e4670ac2b5fb470c8a30d69284c8bde778
Evelyn Graetz <evelyn.graetz@gmx.de>
2019-11-16 16:58:06 +0100

    login and signup styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,63 @@
-import React from 'react';
-import ProjectList from './components/ProjectList';
-import Signup from './components/Signup';
-import { Redirect, Switch, Route } from 'react-router-dom'
-import Login from './components/Login';
-
+import React from "react";
+import ProjectList from "./components/ProjectList";
+import Signup from "./components/Signup";
+import { Redirect, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import ShinyLogin from "./components/ShinyLogin";
+import ShinySignUp from "./components/ShinySignUp";
 
 class App extends React.Component {
-
   state = {
     loggedInUser: this.props.user
-  }
+  };
 
-  updateUserHandler = (userObj) => {
+  updateUserHandler = userObj => {
     this.setState({
       loggedInUser: userObj
-    })
-  }
+    });
+  };
 
   render() {
     return (
       <div>
-        Hello, {this.state.loggedInUser ? this.state.loggedInUser.email : 'Stranger'} !
         <Switch>
           <Route path="/profile" component={ProjectList}></Route>
-          <Route path="/profile" render={() => <ProjectList></ProjectList>}></Route>
-          <Route path="/signup" render={
-            () => {
+          <Route
+            path="/profile"
+            render={() => <ProjectList></ProjectList>}
+          ></Route>
+          <Route
+            path="/signup"
+            render={() => {
               if (this.state.loggedInUser) {
-                return <Redirect to="/profile"></Redirect>
+                return <Redirect to="/profile"></Redirect>;
               } else {
-                return <div>
-                  <Login updateUser={this.updateUserHandler}></Login>
-                  <div> ------------------------ </div>
-                  <Signup history={this.props.history} updateUser={this.updateUserHandler}></Signup>
-                </div>
+                return (
+                  <ShinySignUp
+                    history={this.props.history}
+                    updateUser={this.updateUserHandler}
+                  ></ShinySignUp>
+                );
               }
-            }}>
-          </Route>
+            }}
+          ></Route>
+          <Route
+            path="/login"
+            render={() => {
+              if (this.state.loggedInUser) {
+                return <Redirect to="/profile"></Redirect>;
+              } else {
+                return (
+                  <ShinyLogin
+                    history={this.props.history}
+                    updateUser={this.updateUserHandler}
+                  ></ShinyLogin>
+                );
+              }
+            }}
+          ></Route>
         </Switch>
-      </div >
+      </div>
     );
   }
 }
---
+++ client/src/components/ShinySignUp.js
@@ -0,0 +1,155 @@
+import React from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
+import axios from "axios";
+import { Link as RouterLink } from "react-router-dom";
+
+const withMyStyles = withStyles(theme => ({
+  root: {
+    height: "100vh"
+  },
+  image: {
+    backgroundImage: "url(https://source.unsplash.com/random)",
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center"
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+class ShinyLogin extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  submitHandler = event => {
+    event.preventDefault();
+    axios
+      .post("/api/auth/signup", this.state)
+      .then(response => {
+        this.props.updateUser(response.data);
+        this.props.history.push("/profile");
+      })
+      .catch(() => {});
+  };
+
+  onEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    });
+  };
+
+  onPasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  render() {
+    const classes = this.props.classes;
+
+    return (
+      <Grid container component="main" className={classes.root}>
+        <CssBaseline />
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={this.state.email}
+                onChange={this.onEmailChange}
+              />
+              <TextField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={this.state.password}
+                onChange={this.onPasswordChange}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+              <Grid container>
+                <Grid item xs>
+                  <Link
+                    component={RouterLink}
+                    to="/forgotpassword"
+                    variant="body2"
+                  >
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link component={RouterLink} to="/signup" variant="body2">
+                    {"Already have an account? Sign In"}
+                  </Link>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
+    );
+  }
+}
+
+export default withMyStyles(ShinyLogin);
