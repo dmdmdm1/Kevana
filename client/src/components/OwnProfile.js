5ad2611b639d6a1c9a4000572174b4a42786fac6
cbut <c.butenschoen@gmail.com>
2019-12-11 14:28:40 +0100

    fixes followed users not being shown in own profile

--- client/src/components/OwnProfile.js
+++ client/src/components/OwnProfile.js
@@ -1,18 +1,37 @@
 import React from "react";
-import ShowProfileData from "./ShowProfileData"
+import ShowProfileData from "./ShowProfileData";
+import axios from "axios";
 
 class OwnProfile extends React.Component {
-    state = {
-      userData: this.props.theLoggedInUser
-    };
-  
-    render() {
-      return (
-        <div>
-          <ShowProfileData dataToBeShown={this.state.userData} />
-        </div>
-      );
+  state = {
+    userData: this.props.theLoggedInUser,
+    profileData: [],
+    isLoading: true
+  };
+
+  getProfile = () => {
+    axios.get(`/api/profile/${this.state.userData._id}`).then(response => {
+      this.setState({ profileData: response.data, isLoading: false });
+    });
+  };
+
+  render() {
+    if (this.state.profileData.length === 0) {
+      this.getProfile();
+      return <h1>Loading.....</h1>;
     }
+    return (
+      <div>
+        {this.state.isLoading ? (
+          <p>loading...</p>
+        ) : (
+          <div>
+            <ShowProfileData dataToBeShown={this.state.profileData} />
+          </div>
+        )}
+      </div>
+    );
   }
+}
 
 export default OwnProfile;
