b3dbe46e91d7e7adb55302e27064fcdf1925908b
Dayana Mick <dayanamick@gmail.com>
2019-12-12 14:12:01 +0100

    style profile a bit

--- client/src/components/ShowProfileData.js
+++ client/src/components/ShowProfileData.js
@@ -4,30 +4,29 @@ import { Link } from "react-router-dom";
 function ShowProfileData(props) {
   const { userName, favorites, follows, email } = props.dataToBeShown;
   return (
-    <div>
-      {/* {console.log(follows)} */}
-      {/* <p>Username: {userName}</p> */}
-      {/* Favorites:{" "}
-      {favorites.length ? (
-        <p>there is at least one favorite</p>
-      ) : (
-        <p>there are no favorites yet</p>
-      )} */}
-      User {email}
-      {follows.length ? (
-        <div>
-          is following these users:{" "}
-          {follows.map(followedUser => (
-            <div key={followedUser._id}>
-              <Link to={`/profile/${followedUser._id}`}>
-                {followedUser.email}{" "}
-              </Link>
-            </div>
-          ))}
-        </div>
-      ) : (
-        <p>is not following anyone</p>
-      )}
+    <div class="profile-data">
+      <div className="add-card">
+        <h4>User {email}</h4>
+        {follows.length ? (
+          <div>
+            is following these users:{" "}
+            <ul class="list-group">
+              {follows.map(followedUser => (
+                <div key={followedUser._id}>
+                  <Link
+                    class="list-group-item"
+                    to={`/profile/${followedUser._id}`}
+                  >
+                    {followedUser.email}{" "}
+                  </Link>
+                </div>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>...is not following anyone at the moment. </p>
+        )}
+      </div>
     </div>
   );
 }
